92758eb4f15ab6ea56317fa757469d48f47b2dee
Matteo Papadopoulos <spleenteo@gmail.com>
2012-09-30 01:48:45 +0200

    reorganized js dirs

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,15 +1,3 @@
-function toggler_setup(jq) {
-    jq = jq || document;
-    $('.toggler', jq).click(function(e) {
-        e.preventDefault();
-        var target = this.rel ? $(this.rel) : $(this).next();
-        target.slideToggle();
-    });
-}
-
-
-
 $(document).ready(function() {
-    toggler_setup();
-       
+
 });
