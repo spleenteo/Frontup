2282fc355a9c78115ceef047a1054fed4cdc48e1
Matteo Papadopoulos <spleenteo@gmail.com>
2012-10-26 21:58:33 +0200

    reset the config to works with compass

--- config.rb
+++ config.rb
@@ -3,9 +3,9 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "./"
+sass_dir = "stylesheets"
 images_dir = "css/img"
-javascripts_dir = "js"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -16,7 +16,6 @@ relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
